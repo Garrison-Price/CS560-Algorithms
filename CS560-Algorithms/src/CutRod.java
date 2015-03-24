60ffdfc632aec84a9e209e3486ca9689d332326c
Garrison Price <garrison.b.price@gmail.com>
2015-03-24 12:27:41 -0700

    Added Glue Rod

--- CS560-Algorithms/src/CutRod.java
+++ CS560-Algorithms/src/CutRod.java
@@ -28,18 +28,40 @@ public class CutRod {
 		return bestPriceForLength[n];
 	}
 	
+	public static int memoizedGlueRod(int n) {
+		int[] bestPriceForLength = new int[n+1];
+		
+		bestPriceForLength[0] = 0;
+		
+		for(int currentTotalRodLength = 1; currentTotalRodLength <= n; currentTotalRodLength++) {
+			int bestPrice = INFINITY;
+			
+			for(int currentPartialRodLength = currentTotalRodLength; currentPartialRodLength >= 1; currentPartialRodLength--) {
+				int currentCut = 0;
+				if(pricePerLength[currentPartialRodLength] + bestPriceForLength[currentTotalRodLength-currentPartialRodLength] < bestPrice) {
+					System.arraycopy(optimalCuts[currentTotalRodLength-currentPartialRodLength], 0, optimalCuts[currentTotalRodLength], 0, 9);
+					while(optimalCuts[currentTotalRodLength][currentCut++] != 0);
+					optimalCuts[currentTotalRodLength][--currentCut] = currentPartialRodLength;
+				}
+				bestPrice = Math.min(bestPrice, pricePerLength[currentPartialRodLength] + bestPriceForLength[currentTotalRodLength-currentPartialRodLength]);
+			}
+			bestPriceForLength[currentTotalRodLength] = bestPrice;
+		}
+		return bestPriceForLength[n];
+	}
+	
 	public static void main(String args[]) {
 		pricePerLength[1] = 6;
 		pricePerLength[2] = 9;
 		pricePerLength[3] = 14;
-		pricePerLength[4] = 45;
+		pricePerLength[4] = 20;
 		pricePerLength[5] = 26;
 		pricePerLength[6] = 29;
 		pricePerLength[7] = 35;
 		pricePerLength[8] = 40;
 		pricePerLength[9] = 43;
 		
-		System.out.println(memoizedCutRod(9));
+		System.out.println(memoizedGlueRod(9));
 		System.out.println(Arrays.toString(optimalCuts[9]));
 	}
 }
