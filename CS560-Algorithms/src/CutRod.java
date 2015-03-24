1025b15268846be36c974f4b271156908bd0de93
Garrison Price <garrison.b.price@gmail.com>
2015-03-23 23:33:52 -0700

    Figured out Rod Cut and the storage of which lengths were cut

--- CS560-Algorithms/src/CutRod.java
+++ CS560-Algorithms/src/CutRod.java
@@ -1,4 +1,45 @@
+import java.util.Arrays;
+
 
 public class CutRod {
-
+	static int[] pricePerLength = new int[10];
+	static int[][] optimalCuts = new int[10][9];
+	static final int INFINITY = 9999;
+	
+	public static int memoizedCutRod(int n) {
+		int[] bestPriceForLength = new int[n+1];
+		
+		bestPriceForLength[0] = 0;
+		
+		for(int currentTotalRodLength = 1; currentTotalRodLength <= n; currentTotalRodLength++) {
+			int bestPrice = -INFINITY;
+			
+			for(int currentPartialRodLength = 1; currentPartialRodLength <= currentTotalRodLength; currentPartialRodLength++) {
+				int currentCut = 0;
+				if(pricePerLength[currentPartialRodLength] + bestPriceForLength[currentTotalRodLength-currentPartialRodLength] > bestPrice) {
+					System.arraycopy(optimalCuts[currentTotalRodLength-currentPartialRodLength], 0, optimalCuts[currentTotalRodLength], 0, 9);
+					while(optimalCuts[currentTotalRodLength][currentCut++] != 0);
+					optimalCuts[currentTotalRodLength][--currentCut] = currentPartialRodLength;
+				}
+				bestPrice = Math.max(bestPrice, pricePerLength[currentPartialRodLength] + bestPriceForLength[currentTotalRodLength-currentPartialRodLength]);
+			}
+			bestPriceForLength[currentTotalRodLength] = bestPrice;
+		}
+		return bestPriceForLength[n];
+	}
+	
+	public static void main(String args[]) {
+		pricePerLength[1] = 6;
+		pricePerLength[2] = 9;
+		pricePerLength[3] = 14;
+		pricePerLength[4] = 45;
+		pricePerLength[5] = 26;
+		pricePerLength[6] = 29;
+		pricePerLength[7] = 35;
+		pricePerLength[8] = 40;
+		pricePerLength[9] = 43;
+		
+		System.out.println(memoizedCutRod(9));
+		System.out.println(Arrays.toString(optimalCuts[9]));
+	}
 }
